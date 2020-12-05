618829778b3a353106aaaee2a5c4d739d8cbfc56
zhouyang <870052861@qq.com>
2020-12-06 00:00:39 +0800

    init

---
+++ src/main/java/leetcode/editor/cn/NumberOfIslands.java
@@ -0,0 +1,91 @@
+package leetcode.editor.cn;
+
+public class NumberOfIslands{
+
+    public static void main(String[]args){
+        Solution solution= new NumberOfIslands().new Solution();
+        System.out.println(solution.numIslands(new char[][]{{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}}));
+    }
+    
+    //NO.200
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                if(grid[i][j] == '1'){
+                    depthFirst(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void depthFirst(char[][] grid, int i, int j){
+        grid[i][j] = '0';
+        if(j + 1 < grid[i].length && grid[i][j+1] == '1'){
+            depthFirst(grid, i, j+1);
+        }
+        if(j > 0 && grid[i][j-1] == '1'){
+            depthFirst(grid, i, j-1);
+        }
+        if(i > 0 && grid[i-1][j] == '1'){
+            depthFirst(grid, i-1, j);
+        }
+        if(i+1 < grid.length && grid[i+1][j] == '1'){
+            depthFirst(grid, i+1, j);
+        }
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。 
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。 
+//
+// 此外，你可以假设该网格的四条边均被水包围。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：grid = [
+//  ['1','1','1','1','0'],
+//  ['1','1','0','1','0'],
+//  ['1','1','0','0','0'],
+//  ['0','0','0','0','0']
+//]
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：grid = [
+//  ['1','1','0','0','0'],
+//  ['1','1','0','0','0'],
+//  ['0','0','1','0','0'],
+//  ['0','0','0','1','1']
+//]
+//输出：3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 300 
+// grid[i][j] 的值为 '0' 或 '1' 
+// 
+// Related Topics 深度优先搜索 广度优先搜索 并查集 
+// 👍 885 👎 0
+
+}
